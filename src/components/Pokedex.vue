1378707e5518bc4f37404a0edce7919b4e36e86d
Davi <davi.edfisica@outlook.com>
2025-05-12 21:45:55 +0100

    Setup inicial: estrutura base e index do projeto

---
+++ src/components/Pokedex.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Minha Pokédex</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Pokedex",
+};
+</script>
+
+<style scoped>
+h1 {
+  color: red;
+}
+</style>
